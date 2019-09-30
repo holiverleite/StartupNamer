08901103d755c0b5af2ff09d653264b63e69c0e7
Haroldo Oliveira de Almeida Leite <holiverleite@gmail.com>
2019-09-30 16:03:01 -0300

    Implementation to the app load a package of english words;

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome',
       home: Scaffold(
@@ -12,9 +14,24 @@ class MyApp extends StatelessWidget {
           title: Text("Welcome to Flutter"),
         ),
         body: Center(
-          child: Text("Hello World"),
+          // child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
   }
 }
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
