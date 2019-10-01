3d1a19436ee4d323e43f040eacce6ead24c75f27
Haroldo Oliveira de Almeida Leite <holiverleite@gmail.com>
2019-10-01 15:56:07 -0300

    Added an icon to mark the name as a favorite.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:english_words/english_words.dart' as prefix0;
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
@@ -21,24 +20,46 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+  // final _suggestions = <WordPair>[];
+  // final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = TextStyle(fontSize: 18.0);
+  final Set<WordPair> _saved = Set<WordPair>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Startup Name Generator"),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
   }
 
-  Widget _buildRow(prefix0.WordPair pair) {
+  Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair .asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 
@@ -50,10 +71,16 @@ class RandomWordsState extends State<RandomWords> {
 
       final index = i ~/ 2;
       if (index >= _suggestions.length) {
-        _suggestions.addAll(prefix0.generateWordPairs().take(10));
+        _suggestions.addAll(generateWordPairs().take(10));
       }
       return _buildRow(_suggestions[index]);
     });
+  }
+
+  void _pushSaved() {
+    // Navigator.of(context).push(
+
+    // );
   }
 }
 
